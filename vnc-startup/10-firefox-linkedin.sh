475413da6c9adf1608ffde079b164cb053817327
Byte Block <cnnect@byteblock.io>
2026-03-09 21:31:25 +0530

    vnc

--- vnc-startup/10-firefox-linkedin.sh
+++ vnc-startup/10-firefox-linkedin.sh
@@ -3,6 +3,9 @@
 # Wait for the desktop to be ready
 sleep 10
 
+# Start websockify for noVNC
+websockify --web=/usr/share/novnc 6901 localhost:5900 &
+
 # Open Firefox with LinkedIn login page
 firefox --new-window https://www.linkedin.com/login &
 
